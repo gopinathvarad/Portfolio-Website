488c04a07e7b3cae276b9d8c5e3cd2491b52d8e8
Gopinath <gvarad2001@gmail.com>
2024-09-22 17:57:53 +0100

    Contact, Hero, Projects (created and updated)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/Gopinathpp.jpeg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
@@ -7,10 +17,15 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-6 lg:text-8xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-6 lg:text-7xl"
+            >
               Gopinath Varadarajan
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
